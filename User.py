7b1fc0be5e09343e317db0e1030ec15a32711e4f
maureen-james <manjax97@gmail.com>
2022-04-24 00:22:58 +0300

    generate a save object

--- User.py
+++ User.py
@@ -1,7 +1,7 @@
 class User:
      """
      stores users info.
-     
+
      """
      user_info = []
      def __init__(self,f_name,l_name,p_number):
@@ -9,3 +9,8 @@ class User:
        self.l_name=l_name
        self.p_number=p_number
       
+     def save_user(self):
+      """
+      saves user infomation.
+      """  
+     User.user_info.append(self)
