a7f921ff8ddcb0b866bb749b5d5fa4552ecd0bed
maureen-james <manjax97@gmail.com>
2022-04-23 18:55:17 +0300

    import users unittest

--- User.py
+++ User.py
@@ -1,11 +1,11 @@
 class User:
-
      """
      stores users info.
+     
      """
      user_info = []
      def __init__(self,f_name,l_name,p_number):
-      self.f_name=f_name
-      self.l_name=l_name
-      self.p_number=p_number
+       self.f_name=f_name
+       self.l_name=l_name
+       self.p_number=p_number
       
